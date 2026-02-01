f8b2465991a9dcab8f35a24452afcd4b1244d691
Karima002 <karima.mouadi@hva.nl>
2026-02-01 18:54:35 +0100

    #7 started with homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Header from '../components/Header.svelte';
+    import background from '$lib/assets/background-image.svg';
+</script>
+
+<Header />
+
+<main>
+    <img src="{background}" alt="image of a dark green geometric pattern" />
+</main>
+
+<style>
+    :global(body) {
+        background-color: black;
+    }
+</style>
