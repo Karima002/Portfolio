e96008c5cc2a9159f12d48c375642d2a1233af05
Karima002 <karima.mouadi@hva.nl>
2026-02-01 18:53:44 +0100

    #6 added header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+<header>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">[Projects]</a></li>
+        <li><a href="/contact">[Contact me]</a></li>
+    </ul>
+</header>
+
+<style>
+    ul {
+        display: flex;
+        list-style: none;
+        gap: 1rem;
+        justify-content: space-evenly;
+        gap: 10em;
+    }
+
+    
+</style>
