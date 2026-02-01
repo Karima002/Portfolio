6cdd09caf38f5c37543cda69a768edac8c852202
Karima002 <karima.mouadi@hva.nl>
2026-02-01 22:31:44 +0100

    #6 added styling to header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,16 @@
 <header>
     <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">[Projects]</a></li>
-        <li><a href="/contact">[Contact me]</a></li>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        
+        <li>
+            <a href="/about">[Projects]</a>
+        </li>
+
+        <li>
+            <a href="/contact">[Contact me]</a>
+        </li>
     </ul>
 </header>
 
@@ -15,5 +23,13 @@
         gap: 10em;
     }
 
-    
+    a {
+        text-decoration: none;
+        font-family: var(--font-text);
+        font-size: var(--font-size-xl);
+        color: var(--neutral-color-white);
+        font-weight: 100;
+        text-transform: uppercase;
+    }
+
 </style>
