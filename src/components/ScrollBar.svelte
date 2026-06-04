a5960ddb45f461715749e4148804affee086b4e7
Karima002 <karima.mouadi@hva.nl>
2026-06-04 20:23:39 +0200

    feat: added scrollbar #7

---
+++ src/components/ScrollBar.svelte
@@ -0,0 +1,26 @@
+<div class="scroll-bar"></div>
+
+<style>
+    .scroll-bar {
+        @media (prefers-reduced-motion: no-preference) {
+            height: .8em;
+            position: fixed;
+            top: 0;
+            background-color: var(--accent-color-darkgreen);
+            width: 100%;
+            z-index: 1;
+            transform-origin: left;
+            scale: 0 1;
+
+            animation: scroll linear;
+            animation-timeline: scroll(y);
+        }
+    }
+
+    @keyframes scroll {
+        to {
+            scale: 1 1;
+        }
+    }
+
+</style>
