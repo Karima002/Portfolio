9934c0be6de4612d45668bf2323e5e3d7e47462d
Karima002 <karima.mouadi@hva.nl>
2026-06-06 22:03:27 +0200

    feat: added translations #15

--- src/lib/translations.js
+++ src/lib/translations.js
@@ -1,17 +1,77 @@
 export default {
   en: {
+    // Homepage
+    "header-home": "Home",
+    "header-projects": "Projects",
+    "header-contact": "Contact",
     "homepage.title": "Karima Mouadi",
     "homepage.function": "Frontend Design and Developer",
     "scroll-down": "↓ Scroll down to explore",
+    "horizontal-scroll": "Web development and design with clean code, structure and creativity",
+    "areas-of-expertise": "Areas of expertise",
+    "expertise-1": "Frontend development",
+    "expertise-1-detail": "UX / UI / Figma / Affinity / Adobe Indesign / Adobe Illustrator",
+    "expertise-2": "Frameworks",
+    "expertise-2-detail": "Astro / Sveltekit / 11ty / Nuxt JS",
+    "expertise-3": "Web development",
+    "expertise-3-detail": "HTML / CSS / JavaScript / GSAP / NodeJS",
+    "outro-portfolio": "Portfolio",
+
+    //Projects page
+    "projects-title": "Projects",
+
+    //contact page
+    "contact-description": "I’m Karima, a frontend designer and developer based in Amsterdam. Specializing in building modern and user-friendly websites. With a strong focus on detail and accessibility I transform ideas into powerful digital experiences. As a master’s degree student in International Technology law and Internet law & IE I also build websites in compliance with current European and International legislation. Combining creativity with research, I create websites that align with the needs of end-users",
+    "email": "email"
   },
+
   es: {
+    // Homepage
+    "header-home": "Inicio",
+    "header-projects": "Proyectos",
+    "header-contact": "Contacto",
     "homepage.title": "Karima Mouadi",
     "homepage.function": "Diseñadora y Desarrolladora Frontend",
     "scroll-down": "↓ Desplácese hacia abajo para explorar",
+    "horizontal-scroll": "Desarrollo web y diseño con código limpio, estructura y creatividad",
+    "areas-of-expertise": "Áreas de especialización",
+    "expertise-1": "Desarrollo frontend",
+    "expertise-1-detail": "UX / UI / Figma / Affinity / Adobe Indesign / Adobe Illustrator",
+    "expertise-2": "Frameworks",
+    "expertise-2-detail": "Astro / Sveltekit / 11ty / Nuxt JS",
+    "expertise-3": "Desarrollo web",
+    "expertise-3-detail": "HTML / CSS / JavaScript / GSAP / NodeJS",
+    "outro-portfolio": "Portafolio",
+
+    //Projects
+    "projects-title": "Proyectos",
+
+    //contact
+    "contact-description": "Soy Karima, una diseñadora y desarrolladora frontend con sede en Ámsterdam. Especializada en construir sitios web modernos y fáciles de usar. Con un fuerte enfoque en el detalle y la accesibilidad, transformo ideas en poderosas experiencias digitales. Como estudiante de maestría en Derecho Internacional de la Tecnología y Derecho de Internet y Propiedad Intelectual, también construyo sitios web en cumplimiento con la legislación europea e internacional actual. Combinando creatividad con investigación, creo sitios web que se alinean con las necesidades de los usuarios finales.",
+    "email": "Correo electrónico"
   },
+
   ar: {
+    // Homepage
+    "header-home": "الرئيسية",
+    "header-projects": "المشاريع",
+    "header-contact": "اتصل بي",
     "homepage.title": "كريمة معوادي",
     "homepage.function": "مصممة ومطورة واجهات أمامية",
     "scroll-down": "↓ قم بالتمرير لأسفل للاستكشاف",
-  }
-};
+    "horizontal-scroll": "تطوير وتصميم الويب مع كود نظيف، هيكل وإبداع",
+    "areas-of-expertise": "مجالات الخبرة",
+    "expertise-1": "تطوير الواجهات الأمامية",
+    "expertise-1-detail": "UX / UI / Figma / Affinity / Adobe Indesign / Adobe Illustrator",
+    "expertise-2": "الأطر",
+    "expertise-2-detail": "Astro / Sveltekit / 11ty / Nuxt JS",
+    "expertise-3": "تطوير الويب",
+    "expertise-3-detail": "HTML / CSS / JavaScript / GSAP / NodeJS",
+    "outro-portfolio": "محفظة",
+
+    //projects
+    "projects-title": "المشاريع",
+    //contact
+    "contact-description": "أنا كريمة، مصممة ومطوّرة مواقع إلكترونية مقيمة في أمستردام. أتخصص في إنشاء مواقع إلكترونية حديثة وسهلة الاستخدام، مع اهتمام كبير بالتفاصيل وإمكانية الوصول الرقمي، حيث أحوّل الأفكار إلى تجارب رقمية قوية وفعّالة. وبصفتي طالبة ماجستير في قانون التكنولوجيا الدولي وقانون الإنترنت والملكية الفكرية، أحرص أيضًا على تطوير مواقع إلكترونية متوافقة مع التشريعات الأوروبية والدولية المعمول بها. ومن خلال الجمع بين الإبداع والبحث، أُنشئ مواقع إلكترونية تتوافق مع احتياجات المستخدمين النهائيين وتوفر لهم تجربة استخدام متميزة.",
+    "email": "البريد الإلكتروني",
+  }};
