ff9a1a7b209a3a1b7fc1fbb6da55e05f8671b81a
Karima002 <karima.mouadi@hva.nl>
2026-06-06 20:55:46 +0200

    feat: added basic translations #15

---
+++ src/lib/translations.js
@@ -0,0 +1,17 @@
+export default {
+  en: {
+    "homepage.title": "Karima Mouadi",
+    "homepage.function": "Frontend Design and Developer",
+    "scroll-down": "↓ Scroll down to explore",
+  },
+  es: {
+    "homepage.title": "Karima Mouadi",
+    "homepage.function": "Diseñadora y Desarrolladora Frontend",
+    "scroll-down": "↓ Desplácese hacia abajo para explorar",
+  },
+  ar: {
+    "homepage.title": "كريمة معوادي",
+    "homepage.function": "مصممة ومطورة واجهات أمامية",
+    "scroll-down": "↓ قم بالتمرير لأسفل للاستكشاف",
+  }
+};
